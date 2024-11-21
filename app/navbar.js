7833fa472a01b65ccf5edd668b6de7f57eb34c0e
chintun95 <chintun95@gmail.com>
2024-11-21 11:00:11 -0600

    update

--- app/navbar.js
+++ app/navbar.js
@@ -1,12 +1,8 @@
 'use client';
 import { useState } from "react";
-import { Poppins } from 'next/font/google'
 import './globals.css'
-
-const poppins = Poppins({
-  subsets: ['latin'],
-  weight: ['400', '500', '600', '700'],
-});
+import Image from "next/image";
+import { poppins } from "./fonts/fonts";
 
 export default function NavBar(){
   const [menuOpen, setMenuOpen] = useState(false);
@@ -14,10 +10,10 @@ export default function NavBar(){
   const toggleMenu = () => {
     setMenuOpen((prevState) => !prevState);
   }
-  
+
   return (
     <body className={poppins.className}>
-      <nav id ="deskstop-nav">
+      <nav id ="deskstop-nav" >
         <div className="logo">Chin Lun Wong</div>
         <div>
           <ul className="nav-links">
@@ -44,6 +40,59 @@ export default function NavBar(){
           </div>
         </div>
       </nav>
+      <section id='profile'>
+        <div className="section__pic-container">
+          <Image src="/images/UNT-student-ID-photo.jpg" alt="Chin Lun Wong profile picture"
+          width={300} height={300}/>
+        </div>
+        <div className="section__text">
+          <p className="section__text_p1">Hello, I'm</p>
+          <h1 className="title">Chin Lun Wong</h1>
+          <p className="section__text_p2">Frontend Developer</p>
+          <div className="btn-container">
+            <button className="btn btn-color-2"
+             onClick={() => window.open("/Latest-Resume.pdf")}
+              >Download CV</button>
+            <button className="btn btn-color-1"
+             onClick={() => location.href="./#contact"}
+              >Contact Info</button>
+          </div>
+          <div id="socials-container">
+            <img src="/images/linkedin.png" alt="My linkedin profile" className="icon"
+            onClick={() => location.href="https://www.linkedin.com/in/chin-lun-wong-b57a591b2/" }/>
+            <img src="/images/Github.png" alt="My Github profile" className="icon"
+            onClick={() => location.href="https://github.com/chintun95" }/>
+          </div>
+        </div>
+      </section>  
+      <section id= "about">
+        <p className="section__text_p1">Get to know more</p>
+        <h1 className="title">About Me</h1>
+        <div className="section-container">
+          <div className="section__pic-container">
+            <Image src="/images/UNT-student-ID-photo.jpg" width={300} height={300} alt="Chin Lun Wong profile picture"/>
+          </div>
+          <div className="about-details-container">
+            <div className="about-containers">
+              <div className="details-container">
+                <Image src="/images/experience.png" alt="Experience icon"
+                width={50} height={50}/>
+                <h3>Experience</h3>
+                <p2>2+ years <br/> Frontend Development</p2>
+              </div>
+              <div className="details-container">
+                <Image src="/images/experience.png" alt="Experience icon"
+                width={50} height={50}/>
+                <h3>Education</h3>
+                <p2>Bachelor of Science in <br/> Computer Science</p2>
+              </div>
+            </div>
+            <div className="">
+
+            </div>
+          </div>
+        </div>
+      </section>
     </body>
   )
 }
---
+++ app/profile.js
@@ -0,0 +1,32 @@
+import { poppins } from "./fonts/fonts";
+import Image from "next/image";
+
+export default function Profile(){
+  return(
+    <section id='profile'>
+      <div className="section__pic-container">
+        <Image src="/images/UNT-student-ID-photo.jpg" alt="Chin Lun Wong profile picture"
+        width={300} height={300}/>
+      </div>
+      <div className="section__text">
+        <p className="section__text_p1">Hello, I'm</p>
+        <h1 className="title">Chin Lun Wong</h1>
+        <p className="section__text_p2">Frontend Developer</p>
+        <div className="btn-container">
+          <button className="btn btn-color-2"
+          onClick={() => window.open("/Latest-Resume.pdf")}
+            >Download CV</button>
+          <button className="btn btn-color-1"
+          onClick={() => location.href="./#contact"}
+            >Contact Info</button>
+        </div>
+        <div id="socials-container">
+          <img src="/images/linkedin.png" alt="My linkedin profile" className="icon"
+          onClick={() => location.href="https://www.linkedin.com/in/chin-lun-wong-b57a591b2/" }/>
+          <img src="/images/Github.png" alt="My Github profile" className="icon"
+          onClick={() => location.href="https://github.com/chintun95" }/>
+        </div>
+      </div>
+    </section>
+  );
+}
